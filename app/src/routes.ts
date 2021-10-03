9228300edbc3e434c0e8e7b09bee67b82add35f1
Nate <natnaelgetenew@gmail.com>
2021-10-03 23:58:10 +0300

    Revert "Site  structure with route and material components"

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,49 +1,53 @@
-// @ts-nocheck
 
-import { AccountPage } from "./pages/Account";
-import { GamesStorePage } from "./pages/GamesStore";
-import { InventoryPage } from "./pages/Inventory";
-import { LibraryPage } from "./pages/Library";
-import { MarketplacePage } from "./pages/Marketplace";
+import { AccountPage } from './pages/Account';
+import { GamesStorePage } from './pages/GamesStore';
+import { InventoryPage } from './pages/Inventory';
+import { LibraryPage } from './pages/Library';
+import { MarketplacePage } from './pages/Marketplace';
 
-import PlayersDemo from "./demos/Demo1_players";
+import PlayersDemo from './demos/Demo1_players'
 
 export type Route = {
-  path: string;
-  name: string;
-  component: () => JSX.Element;
-};
+    path: string;
+    name: string;
+    component: () => JSX.Element;
+}
 
 export const routes: Route[] = [
-  {
-    path: "/account",
-    name: "Account",
-    component: AccountPage,
-  },
-  {
-    path: "/gamesStore",
-    name: "Games Store",
-    component: GamesStorePage,
-  },
-  {
-    path: "/inventory",
-    name: "Inventory",
-    component: InventoryPage,
-  },
-  {
-    path: "/library",
-    name: "Library",
-    component: LibraryPage,
-  },
-  {
-    path: "/marketplace",
-    name: "Marketplace",
-    component: MarketplacePage,
-  },
-  {
-    path: "/demo1",
-    name: "Demo 1 (Players)",
-    component: PlayersDemo,
-  },
-];
-
+    // Tmp
+    {
+        path: "/",
+        name: "Home",
+        component: GamesStorePage
+    },
+    {
+        path: "/account",
+        name: "Account",
+        component: AccountPage
+    },
+    {
+        path: "/gamesStore",
+        name: "Games Store",
+        component: GamesStorePage
+    },
+    {
+        path: "/inventory",
+        name: "Inventory",
+        component: InventoryPage
+    },
+    {
+        path: "/library",
+        name: "Library",
+        component: LibraryPage
+    },
+    {
+        path: "/marketplace",
+        name: "Marketplace",
+        component: MarketplacePage
+    },
+    {
+        path: "/demo1",
+        name: "[DEV] Demo 1 (Players)",
+        component: PlayersDemo
+    }
+]
